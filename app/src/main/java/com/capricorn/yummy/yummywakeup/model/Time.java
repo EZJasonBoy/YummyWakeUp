4db0c46a0c516c0d4c1e0f860a19b78ac680f547
DONG <dongchuan55@gmail.com>
2015-08-04 23:21:45 +0200

    Add  Model Time and small fixs

---
+++ app/src/main/java/com/capricorn/yummy/yummywakeup/model/Time.java
@@ -0,0 +1,109 @@
+package com.capricorn.yummy.yummywakeup.model;
+
+import java.util.Calendar;
+
+/**
+ * Created by Chuan on 8/4/2015.
+ */
+public class Time {
+
+    private int hour;
+    private int min;
+    private int month;
+    private int weekDay;
+    private int day;
+    private String timeLabel = "";
+    private String weekMonthDayLabel = "";
+    private Calendar date;
+
+    public Time(long time) {
+
+        date = Calendar.getInstance();
+        date.setTimeInMillis(time);
+
+        this.month = date.get(Calendar.MONTH) + 1;
+        this.day = date.get(Calendar.DAY_OF_MONTH);
+        this.weekDay = date.get(Calendar.DAY_OF_WEEK);
+        this.hour = date.get(Calendar.HOUR_OF_DAY);
+        this.min = date.get(Calendar.MINUTE);
+
+        timeLabel = TimeToString();
+        weekMonthDayLabel = WeekDayToString() + ", " + MonthToString() + " " + this.day;
+
+    }
+
+    private String TimeToString() {
+        return this.hour + ":" + this.min;
+    }
+
+    private String MonthToString() {
+        String month = "";
+        switch (this.month) {
+            case Calendar.JANUARY:
+                month = "January";
+                break;
+            case Calendar.FEBRUARY:
+                month = "February";
+                break;
+            case Calendar.MARCH:
+                month = "March";
+                break;
+            case Calendar.APRIL:
+                month = "April";
+                break;
+            case Calendar.MAY:
+                month = "May";
+                break;
+            case Calendar.JUNE:
+                month = "June";
+                break;
+            case Calendar.JULY:
+                month = "July";
+                break;
+            case Calendar.AUGUST:
+                month = "August";
+                break;
+            case Calendar.SEPTEMBER:
+                month = "September";
+                break;
+            case Calendar.OCTOBER:
+                month = "October";
+                break;
+            case Calendar.NOVEMBER:
+                month = "November";
+                break;
+            case Calendar.DECEMBER:
+                month = "December";
+                break;
+        }
+        return month;
+    }
+
+    private String WeekDayToString() {
+        String weekDay = "";
+        switch (this.weekDay) {
+            case Calendar.MONDAY:
+                weekDay = "Mon";
+                break;
+            case Calendar.TUESDAY:
+                weekDay = "Tue";
+                break;
+            case Calendar.WEDNESDAY:
+                weekDay = "Wed";
+                break;
+            case Calendar.THURSDAY:
+                weekDay = "Thu";
+                break;
+            case Calendar.FRIDAY:
+                weekDay = "Fri";
+                break;
+            case Calendar.SATURDAY:
+                weekDay = "Sat";
+                break;
+            case Calendar.SUNDAY:
+                weekDay = "Sun";
+                break;
+        }
+        return weekDay;
+    }
+}
